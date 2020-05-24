57766b2321dd6ec125495438dd4440148cd6305c
R3ap4r <R3ap4r@telenet.be>
2020-05-24 15:41:10 +0200

    Adding Employee class

---
+++ EmployeeModel.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace MongoDB
+{
+    /// <summary>
+    /// Model for the Employee class
+    /// </summary>
+    public class EmployeeModel
+    {
+        [BsonId]
+        public Guid id { get; set; }
+        [BsonElement("EFirstName")]
+        public string EFirstName { get; set; }
+        [BsonElement("ELastName")]
+        public string ELastName { get; set; }
+        [BsonElement("EmployeeId")]
+        public int EmployeeId { get; set; }
+
+    }
+}
